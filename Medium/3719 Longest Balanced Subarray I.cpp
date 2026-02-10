5d88d107613cdd6a3575132b6be783e7768a24fb
Amit Gupta <aryaamit387@gmail.com>
2026-02-10 19:44:50 +0530

    Solved 3719

---
+++ Medium/3719 Longest Balanced Subarray I.cpp
@@ -0,0 +1,42 @@
+/*
+    Leetcode Link : https://leetcode.com/problems/longest-balanced-subarray-i/
+*/
+
+// TC : O(n)
+// SC : O(n)
+
+class Solution
+{
+public:
+    int longestBalanced(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        int maxL = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            unordered_set<int> even;
+            unordered_set<int> odd;
+
+            for (int j = i; j < n; j++)
+            {
+                if (nums[j] % 2 == 0)
+                {
+                    even.insert(nums[j]);
+                }
+                else
+                {
+                    odd.insert(nums[j]);
+                }
+
+                if (even.size() == odd.size())
+                {
+                    maxL = max(maxL, j - i + 1);
+                }
+            }
+        }
+
+        return maxL;
+    }
+};
