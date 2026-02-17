44821fb01a99372842d48e1a3db157f59f0a1a62
Amit Gupta <aryaamit387@gmail.com>
2026-02-17 23:26:15 +0530

    Solved 401

---
+++ Easy/401 Binary Watch.cpp
@@ -0,0 +1,39 @@
+/*
+    Leetcode Link : https://leetcode.com/problems/binary-watch/
+*/
+
+// TC : O(12*60) = 0(1)
+// SC : O(1)
+
+class Solution
+{
+public:
+    vector<string> readBinaryWatch(int turnedOn)
+    {
+        vector<string> result;
+
+        for (int hour = 0; hour < 12; hour++)
+        {
+            for (int minute = 0; minute < 60; minute++)
+            {
+
+                if (__builtin_popcount(hour) + __builtin_popcount(minute) == turnedOn)
+                {
+
+                    string time = to_string(hour) + ":";
+
+                    if (minute < 10)
+                    {
+                        time += "0";
+                    }
+
+                    time += to_string(minute);
+
+                    result.push_back(time);
+                }
+            }
+        }
+
+        return result;
+    }
+};
