4197399cdc42cee75c453512df1568984d313d61
Amit Gupta <aryaamit387@gmail.com>
2026-02-22 18:19:51 +0530

    Solved 868

---
+++ Easy/868 Binary Gap.cpp
@@ -0,0 +1,29 @@
+/*
+    Leetcode Link : https://leetcode.com/problems/binary-gap/
+*/
+
+// TC : O(1)
+// SC : O(1)
+
+class Solution
+{
+public:
+    int binaryGap(int n)
+    {
+        int curr = 0;
+        int prev = -1;
+
+        int result = 0;
+
+        for (int curr = 0; curr < 32; curr++)
+        {
+            if (((n >> curr) & 1) > 0)
+            {
+                result = (prev != -1) ? max(result, curr - prev) : result;
+                prev = curr;
+            }
+        }
+
+        return result;
+    }
+};
