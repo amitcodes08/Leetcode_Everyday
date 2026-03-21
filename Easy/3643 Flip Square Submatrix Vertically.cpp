4667df78c0d12adba60a646931279a45dff7398e
Amit Gupta <aryaamit387@gmail.com>
2026-03-21 18:41:31 +0530

    Solved 3643

---
+++ Easy/3643 Flip Square Submatrix Vertically.cpp
@@ -0,0 +1,30 @@
+/*
+    Leetcode Link : https://leetcode.com/problems/flip-square-submatrix-vertically/
+*/
+
+// TC : O(m*n)
+// SC : O(1)
+
+class Solution
+{
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>> &grid, int x, int y, int k)
+    {
+        int startRow = x;
+        int endRow = x + k - 1;
+
+        int startCol = y;
+        int endCol = y + k - 1;
+
+        for (int i = startRow; i <= endRow; i++)
+        {
+            for (int j = startCol; j <= endCol; j++)
+            {
+                swap(grid[i][j], grid[endRow][j]);
+            }
+            endRow--;
+        }
+
+        return grid;
+    }
+};
