748b5511deead7a52be929cabdc2ec50a2b7dc1d
Amit Gupta <aryaamit387@gmail.com>
2026-03-29 19:23:32 +0530

    Solved 2839

---
+++ Easy/2839 Check if Strings Can be Made Equal With Operations I.cpp
@@ -0,0 +1,39 @@
+/*
+    Leetcode Link : https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/description/
+*/
+
+// TC : O(1)
+// SC : O(1)
+
+class Solution
+{
+public:
+    bool canBeEqual(string s1, string s2)
+    {
+        vector<int> even(26, 0), odd(26, 0);
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (i % 2 == 0)
+            {
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            }
+            else
+            {
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (even[i] != 0 || odd[i] != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
