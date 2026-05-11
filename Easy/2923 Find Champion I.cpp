7966c0d5a65eee9c9802cc5985e3bc43c371f51e
Amit Gupta <aryaamit387@gmail.com>
2026-05-11 20:54:34 +0530

    Solved 2923

---
+++ Easy/2923 Find Champion I.cpp
@@ -0,0 +1,26 @@
+/*
+    Leetcode Link : https://leetcode.com/problems/find-champion-i/
+*/
+
+// TC : O(n)
+// SC : O(1)
+
+class Solution
+{
+public:
+    int findChampion(vector<vector<int>> &grid)
+    {
+        int winner = 0;
+        int n = grid.size();
+        for (int opponent = 0; opponent < n; ++opponent)
+        {
+            if (opponent == winner)
+                continue;
+            if (grid[winner][opponent] == 0)
+            {
+                winner = opponent;
+            }
+        }
+        return winner;
+    }
+};
