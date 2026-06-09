d8d3f2d0d1d1ca06397003ccc2c22fc07ef6c6b7
Amit Gupta <aryaamit387@gmail.com>
2026-06-09 20:45:55 +0530

    Solved 3689

---
+++ Medium/3689 Maximum Total Subarray Value I.cpp
@@ -0,0 +1,24 @@
+/*
+    LeetCode Link : https://leetcode.com/problems/maximum-total-subarray-value-i/description/
+*/
+
+// Approach (Linear Scan - find max and min)
+// T.C : O(n)
+// S.C : O(1)
+class Solution
+{
+public:
+    long long maxTotalValue(vector<int> &nums, int k)
+    {
+        int maxEl = INT_MIN;
+        int minEl = INT_MAX;
+
+        for (int &num : nums)
+        {
+            maxEl = max(maxEl, num);
+            minEl = min(minEl, num);
+        }
+
+        return 1LL * (maxEl - minEl) * k;
+    }
+};
