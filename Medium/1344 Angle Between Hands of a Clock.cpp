b91aee8e21d6145714fa6c2eb1c652a90fdc6c6e
Amit Gupta <aryaamit387@gmail.com>
2026-06-18 20:27:28 +0530

    Solved 1344

---
+++ Medium/1344 Angle Between Hands of a Clock.cpp
@@ -0,0 +1,9 @@
+/*
+    LeetCode Link : https://leetcode.com/problems/angle-between-hands-of-a-clock/
+*/
+
+// Approach (Mathematical Calculation)
+// T.C : O(1)
+// S.C : O(1)
+
+// will update soln soon
