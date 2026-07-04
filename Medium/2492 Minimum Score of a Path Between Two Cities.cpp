22b2bc4f451e816cf5f5288f31bfb21c55b970b6
Amit Gupta <aryaamit387@gmail.com>
2026-07-04 08:30:45 +0530

    Solved 2492

---
+++ Medium/2492 Minimum Score of a Path Between Two Cities.cpp
@@ -0,0 +1,47 @@
+/*
+    LeetCode Link : https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/description/
+*/
+
+// BFS
+// TC : O(V+E), V = number of vertices, E = number of edges
+// SC : O(V+E)
+
+class Solution
+{
+public:
+    int minScore(int n, vector<vector<int>> &roads)
+    {
+        vector<vector<pair<int, int>>> g(n + 1);
+
+        for (auto &e : roads)
+        {
+            g[e[0]].push_back({e[1], e[2]});
+            g[e[1]].push_back({e[0], e[2]});
+        }
+
+        vector<int> vis(n + 1, 0);
+        queue<int> q;
+        q.push(1);
+        vis[1] = 1;
+
+        int ans = INT_MAX;
+
+        while (!q.empty())
+        {
+            int u = q.front();
+            q.pop();
+
+            for (auto &[v, w] : g[u])
+            {
+                ans = min(ans, w);
+                if (!vis[v])
+                {
+                    vis[v] = 1;
+                    q.push(v);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
